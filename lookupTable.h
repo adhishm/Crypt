18181dd9be5df6a71b084bf0d8f83fc5cfb021d3
Adhish Majumdar <adhishm@gmail.com>
2014-08-19 23:45:01 +0200

    Defined the class LookupTable

---
+++ lookupTable.h
@@ -0,0 +1,46 @@
+#ifndef LOOKUPTABLE_H
+#define LOOKUPTABLE_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#ifndef CAPS_START
+#define CAPS_START 65
+#endif
+
+#ifndef SMALL_START
+#define SMALL_START 97
+#endif
+
+namespace Crypt
+{
+  /**
+   * @brief Class LookupTable to represent a lookup table for the 
+   */
+  class LookupTable
+  {
+  protected:
+    // Protected members
+    vector<char> _sourceLetters;
+    vector<char> _sourceLetters_caps;
+    vector<char> _codeLetters;
+    vector<char> _codeLetters_caps;
+    
+    // Protected functions
+    void _initializeSourceVectors();
+    void _initializeCodeVectors();
+  public:
+    LookupTable();
+    
+    virtual ~LookupTable();
+    
+    bool updateCodeLetter (char codeLetter, char sourceLetter);
+    void displayLookupTable() const;
+  };
+}	// namespace Crypt
+
+#endif
